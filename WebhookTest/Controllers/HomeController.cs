db4ba3ab6f5e8cbab8fb9fc6705cf76a131241f9
kashif <kashif_atiq@hotmail.com>
2016-06-12 18:43:02 +0500

    safai

--- WebhookTest/Controllers/HomeController.cs
+++ WebhookTest/Controllers/HomeController.cs
@@ -13,20 +13,6 @@ namespace WebhookTest.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-        
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [HttpPost]
         public ActionResult Webhook()
         {
